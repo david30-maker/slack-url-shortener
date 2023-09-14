b84d18959c0a0b2c1cc222305bfb592bc458c9b4
david30-maker <davidokpah396@gmail.com>
2023-09-14 21:18:13 +0100

    ADD:implement routes for app path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'shortened_urls#index'
+  get '/:short_url', to: 'shortened_urls#show'
+  post '/shortened_urls', to: 'shortened_urls#create'
 end

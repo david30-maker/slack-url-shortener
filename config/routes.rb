b3840ef757457301f4ae9ea5dce6b175c0d78c11
david30-maker <davidokpah396@gmail.com>
2023-09-14 21:33:10 +0100

    ADD:implememement show method to display shortened_url in shortenedurl controller class

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'shortened_urls#index'
   get '/:short_url', to: 'shortened_urls#show'
-  post '/shortened_urls', to: 'shortened_urls#create'
+  post '/shortened_urls/create', to: 'shortened_urls#create'
 end

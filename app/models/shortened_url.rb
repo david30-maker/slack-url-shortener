a1096c70f3489e6d0a9a2d2114ed1ab866f362f3
david30-maker <davidokpah396@gmail.com>
2023-09-14 23:27:45 +0100

    ADD:implement validation and handle duplicate in models

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,2 +1,9 @@
 class ShortenedUrl < ApplicationRecord
+    validates :original_url, presence: true
+    validates_format_of :original_url,
+    with: /\A(?:(?:https?|ftp):\/\/)?([-a-zA-Z0-9.]{2,256}\.[a-z]{2,4}\b(?:[-a-zA-Z0-9@,!:%_\+.~#?&\/\/=]*)?)\z/
+
+    def find_duplicate
+        ShortenedUrl.find_by_sanitize_url(self.sanitize_url)
+    end
 end
